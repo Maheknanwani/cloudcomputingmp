4773ea9a50693ab4241c23a3e1099817c8b002c4
Mahek Nanwani <114577442+Maheknanwani@users.noreply.github.com>
2024-07-30 12:24:21 +0530

    cc

---
+++ Arguments.java
@@ -0,0 +1,10 @@
+public class Arguments {
+    public static void main(String[] args)
+    {
+        System.out.println("Below are arguments pass to program.");
+        for (String a: args)
+        {
+            System.out.println(a);
+        }
+    }
+}
